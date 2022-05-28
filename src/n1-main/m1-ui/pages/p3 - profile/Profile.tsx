8d8b20926bd6595d6b60b477ba85990b7d9f01dd
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-05-29 01:21:30 +0600

    jsx was added

--- src/n1-main/m1-ui/pages/p3 - profile/Profile.tsx
+++ src/n1-main/m1-ui/pages/p3 - profile/Profile.tsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import s from './profilePage.module.css';
 import SuperButton from '../../common/c2-SuperButton/SuperButton';
 import noPhoto from '../../../../assets/img/noPhoto.png'
 import {ProfileEdit} from './ProfileEdit';
+import {NavLink} from 'react-router-dom';
+import {PATH} from '../../routes/Routs';
 
 export const Profile = () => {
+    const [mode, setMode] = useState(true)
+
+    const changeMode = () => {
+        setMode(!mode)
+    }
+
     return (
         <div className={s.profileContainer}>
-            <div className={s.components}>
+            {mode ? <div className={s.components}>
                 <div className={s.avatar}>
                     <img src={'' || noPhoto} alt={'ava'}/>
 
                     <h2 className={s.profileName}>Your Name</h2>
 
-                    <h3 className={s.profileStatus}>Your Status</h3>
+
                 </div>
-                <SuperButton title={'Edit Profile'}/>
-            </div>
-            <ProfileEdit/>
+                <SuperButton title={'Edit Profile'} onClick={changeMode}/>
+
+                <NavLink to={PATH.LOGIN} className={navData => navData.isActive ? s.active : s.link}>
+                    <span>Log Out</span>
+                </NavLink>
+
+
+
+            </div> : <ProfileEdit changeMode={changeMode}/>}
         </div>
     )
 }
--- src/n1-main/m1-ui/pages/p3 - profile/ProfileEdit.tsx
+++ src/n1-main/m1-ui/pages/p3 - profile/ProfileEdit.tsx
@@ -4,23 +4,28 @@ import SuperButton from '../../common/c2-SuperButton/SuperButton';
 import noPhoto from '../../../../assets/img/noPhoto.png'
 import SuperInputText from '../../common/c1-SuperInputText/SuperInputText';
 
-export const ProfileEdit = () => {
+type ProfileEditType = {
+    changeMode: (mode: boolean) => void
+}
+
+export const ProfileEdit = (props: ProfileEditType) => {
     return (
         <div className={s.profileContainer}>
             <div className={s.components}>
+
                 <div className={s.avatar}>
-
-                    <h2 className={s.profileName}>Personal INFO</h2>
-
+                    <h2 className={s.profileName}>Personal Information</h2>
                     <img src={'' || noPhoto} alt={'ava'}/>
+                </div>
 
+                <div className={s.inputBlock}>
                     <SuperInputText placeholder={'Nickname'}/>
                     <SuperInputText placeholder={'Email'}/>
-
                 </div>
+
                 <div className={s.buttonBlock}>
-                    <SuperButton title={'Cancel'}/>
-                    <SuperButton title={'Save'}/>
+                    <SuperButton className={s.cancelButton} title={'Cancel'} onClick={() => props.changeMode(true)}/>
+                    <SuperButton title={'Save'} onClick={() => props.changeMode(true)}/>
                 </div>
             </div>
         </div>
