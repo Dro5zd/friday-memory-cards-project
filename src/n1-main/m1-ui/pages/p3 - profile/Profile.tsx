e229a0a4fdae147ab989e671a353fbb14b0c0680
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-05-29 23:03:31 +0600

    add login state

--- src/n1-main/m1-ui/pages/p3 - profile/Profile.tsx
+++ src/n1-main/m1-ui/pages/p3 - profile/Profile.tsx
@@ -5,23 +5,23 @@ import noPhoto from '../../../../assets/img/noPhoto.png'
 import {ProfileEdit} from './ProfileEdit';
 import {NavLink} from 'react-router-dom';
 import {PATH} from '../../routes/Routs';
+import {useTypedSelector} from '../../../m2-bll/store';
 
 export const Profile = () => {
     const [mode, setMode] = useState(true)
-
     const changeMode = () => {
         setMode(!mode)
     }
+    const login = useTypedSelector(state => state.auth.name)
 
+    // const dispatch = useDispatch<any>()
     return (
         <div className={s.profileContainer}>
             {mode ? <div className={s.components}>
                 <div className={s.avatar}>
                     <img src={'' || noPhoto} alt={'ava'}/>
 
-                    <h2 className={s.profileName}>Your Name</h2>
-
-
+                    <h2 className={s.profileName}>{login}</h2>
                 </div>
                 <SuperButton title={'Edit Profile'} onClick={changeMode}/>
 
