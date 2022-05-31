278ae4cf6273c2486f4ed22545e51400dbf9ea18
Bogdan <bogdan_mykhailov@icloud.com>
2022-05-31 16:21:21 +0300

    fix preloader

--- src/n1-main/m1-ui/common/c7-Preloader/Preloader.tsx
+++ src/n1-main/m1-ui/common/c7-Preloader/Preloader.tsx
@@ -4,8 +4,9 @@ import preloader from '../../../../assets/img/Dual Ball-1.3s-108px.svg'
 import s from './Preloader.module.css'
 const Preloader = () => {
     return (
-        <div>
+        <div className={s.preloaderWrapper}>
             <img className={s.preloader} src={preloader} alt={'gif'}/>
+          <span>Stand with Ukraine 🇺🇦</span>
         </div>
     );
 };
