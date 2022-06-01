957c4f971f082999399fe37849fbefd68c264369
Bogdan <bogdan_mykhailov@icloud.com>
2022-06-01 11:48:19 +0300

    fix

--- src/n1-main/m1-ui/common/c1-SuperInputText/SuperInputText.tsx
+++ src/n1-main/m1-ui/common/c1-SuperInputText/SuperInputText.tsx
@@ -12,59 +12,59 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperInputTextPropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeText?: (value: string) => void
-    onEnter?: () => void
-    error?: string
-    spanClassName?: string;
-    // onChange?: Dispatch<SetStateAction<string>>;
+  onChangeText?: (value: string) => void
+  onEnter?: () => void
+  error?: string
+  spanClassName?: string;
+  // onChange?: Dispatch<SetStateAction<string>>;
 }
 
 const SuperInputText = React.forwardRef<HTMLInputElement, SuperInputTextPropsType>((
-    {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
-        onChange, onChangeText,
-        onKeyPress, onEnter,
-        error,
-        className, spanClassName,
+  {
+    type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+    onChange, onChangeText,
+    onKeyPress, onEnter,
+    error,
+    className, spanClassName,
 
-        ...restProps// все остальные пропсы попадут в объект restProps
-    }, ref
+    ...restProps// все остальные пропсы попадут в объект restProps
+  }, ref
 ) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange // если есть пропс onChange
-        && onChange(e) // то передать ему е (поскольку onChange не обязателен)
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange // если есть пропс onChange
+    && onChange(e) // то передать ему е (поскольку onChange не обязателен)
 
-        onChangeText && onChangeText(e.currentTarget.value)
-    }
-    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-        onKeyPress && onKeyPress(e);
+    onChangeText && onChangeText(e.currentTarget.value)
+  }
+  const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+    onKeyPress && onKeyPress(e);
 
-        onEnter // если есть пропс onEnter
-        && e.key === 'Enter' // и если нажата кнопка Enter
-        && onEnter() // то вызвать его
-    }
+    onEnter // если есть пропс onEnter
+    && e.key === 'Enter' // и если нажата кнопка Enter
+    && onEnter() // то вызвать его
+  }
 
-    const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
-    const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}` // need to fix
-    // with
-    // (?:) and
-    // s.superInput
+  const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
+  const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}` // need to fix
+  // with
+  // (?:) and
+  // s.superInput
 
-    return (
-        <>
-            <input
-                // type={'text'}
-                type={type}
-                onChange={onChangeCallback}
-                onKeyPress={onKeyPressCallback}
-                className={finalInputClassName}
-                ref={ref}
+  return (
+    <>
+      <input
+        // type={'text'}
+        type={type}
+        onChange={onChangeCallback}
+        onKeyPress={onKeyPressCallback}
+        className={finalInputClassName}
+        ref={ref}
 
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
-            />
-            {error && <span className={finalSpanClassName}>{error}</span>}
-        </>
-    )
+        {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+      />
+      {error && <span className={finalSpanClassName}>{error}</span>}
+    </>
+  )
 })
 
 export default SuperInputText
