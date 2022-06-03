7c0d8ce0e91cd205ab661ad733204f75f73a0583
Bogdan <bogdan_mykhailov@icloud.com>
2022-06-03 14:51:17 +0300

    fix baseURL

--- src/n1-main/m3-dal/auth-api.ts
+++ src/n1-main/m3-dal/auth-api.ts
@@ -2,8 +2,8 @@ import axios, {AxiosResponse} from 'axios';
 import {RecoverPassRequestType} from "../m2-bll/passwordRecoveryReducer";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:7542/2.0/',
-  // baseURL: 'https://neko-back.herokuapp.com/2.0/',
+  // baseURL: 'http://localhost:7542/2.0/',
+  baseURL: 'https://neko-back.herokuapp.com/2.0/',
   withCredentials: true,
   // headers: {
   //   'API-KEY': ''
--- src/n1-main/m3-dal/registration-api.ts
+++ src/n1-main/m3-dal/registration-api.ts
@@ -2,19 +2,20 @@ import axios from 'axios';
 import {RequestRegisterType} from "../m2-bll/registerReducer";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
-    withCredentials: true,
-    headers: {
-        'API-KEY': ''
-    }
+  // baseURL: 'http://localhost:7542/2.0/',
+  baseURL: 'https://neko-back.herokuapp.com/2.0/',
+  withCredentials: true,
+  headers: {
+    'API-KEY': ''
+  }
 })
 
 type RegistrationResponseType = {
-    error?: string
+  error?: string
 }
 
 export const registrationAPI = {
-   async register(data: RequestRegisterType) {
-        return await instance.post<RegistrationResponseType>('/auth/register', data)
-    },
+  async register(data: RequestRegisterType) {
+    return await instance.post<RegistrationResponseType>('/auth/register', data)
+  },
 }
