1aab0128e2fcf799e30410a5c3f9723fb8f9b358
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-06-05 13:56:53 +0600

    cards list css have been updated

--- src/n1-main/m1-ui/pages/p10-cardsList/CardsList.tsx
+++ src/n1-main/m1-ui/pages/p10-cardsList/CardsList.tsx
@@ -1,22 +1,37 @@
 import React from 'react'
 import s from './cardsList.module.css'
 import SuperInputText from '../../common/c1-SuperInputText/SuperInputText';
-import {CardItem} from "./c1-cards/CardItem";
+import {CardItem} from './c1-cards/CardItem';
+import SuperButton from '../../common/c2-SuperButton/SuperButton';
+import {PackItem} from '../p8-packsList/p1-packs/PackItem';
 
 export const CardsList = () => {
 
-  return (
-    <div className={s.container}>
-      <div className={s.components}>
-        <div className={s.packListTitle}>
-          CARDSLIST
-          <SuperInputText/>
-          <SuperInputText/>
-          <CardItem/>
+    return (
+        <div className={s.container}>
+            <div className={s.components}>
+                <div className={s.packSide}>
+                    <h4>PACKS LIST</h4>
+                    <div className={s.searchContainer}>
+                        <SuperInputText className={s.searchInput}/>
+                        <SuperButton title={'ADD NEW PACK'} className={s.searchButton}/>
+                    </div>
+                    <div className={s.packsContainer}>
+                        <div className={s.packListHeader}>
+                            <div className={s.nameTitle}>Question</div>
+                            <div className={s.nameTitle}>Answer</div>
+                            <div className={s.updateTitle}>Last Updated</div>
+                            <div className={s.actionsTitle}>Grade</div>
+                        </div>
+                        <CardItem/>
+                    </div>
+                    <div className={s.paginationContainer}>
+                        123456789
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 
--- src/n1-main/m1-ui/pages/p10-cardsList/c1-cards/CardItem.tsx
+++ src/n1-main/m1-ui/pages/p10-cardsList/c1-cards/CardItem.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import s from './cardItem.module.css';
 
 export const CardItem = () => {
   return (
     <div>
-      <div>
-        <div>Where the russian ship was sent?</div>
-        <div>russian ship was sent to...</div>
-        <div>04.06.2022</div>
-        <div>☆☆☆☆☆</div>
+      <div className={s.cardItemContainer}>
+        <div className={s.nameColumn}>Where the russian ship was sent?</div>
+        <div className={s.nameColumn}>russian ship was sent to...</div>
+        <div className={s.updateColumn}>04.06.2022</div>
+        <div className={s.actionsColumn}>☆☆☆☆☆</div>
       </div>
     </div>
   );
