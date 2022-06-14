2c2a97d408f549108ef907518aeb1335928735ab
Bogdan <bogdan_mykhailov@icloud.com>
2022-06-14 23:01:24 +0300

    fix learn card modal css

--- src/n1-main/m1-ui/pages/p9-cardsList/c1-cards/CardsModals/OpenCardModal/LearnCardModal.tsx
+++ src/n1-main/m1-ui/pages/p9-cardsList/c1-cards/CardsModals/OpenCardModal/LearnCardModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ModalEdited } from '../../../../../common/c14-Modal/ModalEdited';
 import SuperButton from "../../../../../common/c2-SuperButton/SuperButton";
-import s from './OpenCardModal.module.css'
+import s from './LernCardModal.module.css'
 
 interface IOpenCardModal {
     closeModal: () => void;
@@ -15,12 +15,12 @@ export const LearnCardModal: React.FC<IOpenCardModal> = ({closeModal, modalMode,
         <ModalEdited closeModal={closeModal} modalMode={modalMode}>
             <div className={s.container}>
                 <div className={s.block}>
-                    <div className={s.title}>Question</div>
-                    <div className={s.text}>{question}</div>
+                    <div className={s.title}>Question:</div>
+                    <div className={s.titleQuestion}>"{question}"</div>
                 </div>
                 <div className={s.block}>
-                    <div className={s.title}>Answer</div>
-                    <div className={s.text}>{answer}</div>
+                    <div className={s.title}>Answer:</div>
+                    <div className={s.titleAnswer}>"{answer}"</div>
                 </div>
                 <SuperButton className={s.cancelButton} onClick={closeModal} title={'Close'}/>
             </div>
