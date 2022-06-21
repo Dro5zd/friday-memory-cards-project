3af4ad3408c5b44ad5c13501d4dd208d182bfc32
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-06-21 17:36:31 +0600

    first

--- src/n1-main/m1-ui/pages/p3-profile/EditPhotoIcon.tsx
+++ src/n1-main/m1-ui/pages/p3-profile/EditPhotoIcon.tsx
@@ -1,19 +1,26 @@
 import React, {useRef} from 'react';
 import camera from "../../../../assets/img/camera-solid.svg";
 import {InputFile} from "../../common/c1.1-InputFile/InputFile";
+import {updateProfileTC} from '../../../m2-bll/profileReducer';
+import {useTypedDispatch} from '../../../m2-bll/store';
 
 interface IProps {
     className: string;
 }
 
 export const EditPhotoIcon: React.FC<IProps> = ({className}) => {
+
     const myRef = useRef<HTMLInputElement>(null)
-    const onChangeHandler = (value: string) => {
-        console.log(value)
+    const dispatch = useTypedDispatch()
+
+    const onChangeHandler = (avatar: string) => {
+        dispatch(updateProfileTC({avatar: avatar}))
     }
+
     const onClick = () => {
         myRef.current && myRef.current.click()
     }
+
     return (
         <div onClick={onClick} className={className}>
             <img src={camera} alt="camera"/>
